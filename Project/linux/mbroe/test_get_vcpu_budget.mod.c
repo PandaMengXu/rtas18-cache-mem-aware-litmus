159ad7431b5f3bb95d1a7f41b6d7a68731caa91c
Meng Xu <mengxu@cis.upenn.edu>
2016-05-05 01:03:39 -0400

    linux:module:test VCPUOP_get_rt_info hypercall
    A linux module to test the functionality of the added hypercall which
    gets the current remaining budget of a specified VCPU.
    
    This is also a showpiece to demonstrate how the hypercall function
    should be used.
    
    NB:
        Do NOT directly call the HYPERCALL_ function, use the wrapper
    function xen_get_rt_info instead
    
    Test
        Tested and worked
        We can see the period, budget and the current remaining budget of a
    specified VCPU in dom0.
        TODO: I have not tested in domU, although it should also work in
    domU in my mind experiment
    
    Signed-off-by: Meng Xu

---
+++ Project/linux/mbroe/test_get_vcpu_budget.mod.c
@@ -0,0 +1,32 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xe422d0ff, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x677a900d, __VMLINUX_SYMBOL_STR(xen_get_rt_info) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "5A9561E9B3EB2DF37AF640F");
